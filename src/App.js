f7a9415aa91708b71f6d5400e0ebf3fc1ebeb873
janvi01 <janvibajo1@gmail.com>
2024-01-13 21:01:11 +0530

    feat: api setup and image card container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img alt="" src="https://source.unsplash.com/random" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by xyz
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            1000
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            1000
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            1000
+          </li>
+        </ul>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag1
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag2
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag3
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
