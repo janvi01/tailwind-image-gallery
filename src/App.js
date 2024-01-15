af1c557d9d4a54f706bd3288be2f505bac6804d1
janvi01 <janvibajo1@gmail.com>
2024-01-15 12:10:22 +0530

    feat: Image search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
+import SearchImage from "./components/SearchImage";
 
-export default function App() {
+function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("");
@@ -19,16 +20,24 @@ export default function App() {
   }, [term]);
 
   return (
-    <>
+    <div className="container mx-auto flex-col">
+      <SearchImage searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-y-4 place-items-center">
           {images.map((image) => (
             <ImageCard key={image.id} image={image} />
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 }
+
+export default App;
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,8 +4,8 @@ const ImageCard = ({ image }) => {
   const tags = image.tags.split(",");
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full" />
+    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+      <img src={image.webformatURL} alt="" className="w-full h-60" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image.user}
