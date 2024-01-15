58ea350538a8de791f126691224ac7cb99a9a5e9
janvi01 <janvibajo1@gmail.com>
2024-01-15 13:56:59 +0530

    feat: image term clear

--- src/components/SearchImage.js
+++ src/components/SearchImage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
 
 function SearchImage({ searchText }) {
   const [text, setText] = useState("");
@@ -10,17 +11,31 @@ function SearchImage({ searchText }) {
 
   return (
     <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
-      <form onSubmit={onSubmit} className="w-full max-w-sm">
-        <div className="flex items-center border-b-2 border-teal-500 py-2">
+      <form className="w-full max-w-sm">
+        <div className="flex items-center gap-2 border-b-2 border-teal-500 py-2">
           <input
             onChange={(e) => setText(e.target.value)}
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="Search Image Term..."
           />
+          {text ? (
+            <button
+              className="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white p-2 border border-teal-500 hover:border-transparent rounded"
+              title="reset"
+              onClick={() => {
+                searchText("");
+              }}
+            >
+              <MdClose />
+            </button>
+          ) : (
+            ""
+          )}
           <button
             className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
             type="submit"
+            onClick={onSubmit}
           >
             Search
           </button>
