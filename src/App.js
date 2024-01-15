9a5f0ad9d8f4385f23ba7411cc5d8b9ec0a8e5ad
janvi01 <janvibajo1@gmail.com>
2024-01-15 11:52:36 +0530

    feat: Image card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
 export default function App() {
   const [images, setImages] = useState([]);
@@ -18,38 +19,16 @@ export default function App() {
   }, [term]);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img alt="" src="https://source.unsplash.com/random" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by xyz
+    <>
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            1000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            1000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            1000
-          </li>
-        </ul>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag1
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag3
-          </span>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
